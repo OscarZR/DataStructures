b34dfbfe21e2abfa2c162f3e19d84e7fc5b75b80
gaciaza2015 <70037848+gaciaza2015@users.noreply.github.com>
2021-05-24 15:01:17 -0500

    Add files via upload

--- hash_chains1.py
+++ hash_chains1.py
@@ -1,12 +1,43 @@
-#"A la primera" Alejandro el Capo
 '''
-Sean S el arreglo de los códigos ascii de cada una de nuestras letras en el 
-string, hacemos hashing como:
+I this program we built a hash table using the chaining scheme in a
+telephone schedule, we call instructions: add,check,find and del.
+For each instruction "check" we print the array of strings saved at that
+moment in that specific index, and for each "find" instruction we search
+for the calculated hash value of the given string in our table and we print 
+"yes" if its on the table or "no" if not.
+For calculate the hash value of each string:
+Let S be the arrangement of the ascii codes of each of our letters in the
+string, we do hashing like:
     
-    h(S) = ((suma(S[i]*x**i)) mod p) mod m   ,   i de 0 a len(S)-1
+    h(S) = ((sum(S[i]*x**i)) mod p) mod m   ,   i from 0 to len(S)-1
     
-siendo m el tamaño de nuestra tabla hash y p un número primo mayor 
-en este caso p = 1000000007
+where m is the size of our hash table and p is a larger prime number,
+in this case p = 1000000007.
+And our hash table will be a dinamic array of lenght m.
+
+In this script we use a toy example '1.txt':
+5
+12
+add world
+add HellO
+check 4
+find World
+find world
+del world
+check 4
+del HellO
+add luck
+add GooD
+check 2
+del good
+
+And the program must print:
+HellO world
+no
+yes
+HellO 
+GooD luck
+
 '''
 
 def function(ha):
@@ -54,7 +85,7 @@ def hashing(hash_a):
     
     if hash_a[0] == "check":
         mm = int(hash_a[1])
-        b = hashp[mm][::-1]             #Arreglo invertido
+        b = hashp[mm][::-1]             #Inverted array
         return " ".join(b)
     
     else: pass
@@ -76,16 +107,5 @@ if __name__ == '__main__':
             print(it)
     f.close()
     
-    '''
-    m = int(input())
-    n = int(input())
-    hashp = [[] for i in range (m)]
-    hash_ = []
-    for i in range(n):
-        hash_.append(input().split())
-        it = hashing(hash_[i])
-        if it != None:
-            print(it)
-    '''
     
     
